39baa63411586be97dcefb99df0258d795d8b141
biagzi <renata.biazzi@usp.br>
2022-05-18 11:09:50 -0300

    CPPN, Direct Encode, Global Direct Encode genotype structures

--- networks.py
+++ networks.py
@@ -10,7 +10,7 @@ from tools.utils import neg_abs, neg_square, sqrt_abs, neg_sqrt_abs, normalize, 
 
 class OrderedGraph(DiGraph):
     """Create a graph object that tracks the order nodes and their neighbors are added."""
-    node_dict_factory = OrderedDict
+    node_dict_factory = OrderedDict  #R: OrderedDict is a dictionary that keeps the same order as the order you added the key and value
     adjlist_dict_factory = OrderedDict
 
 
@@ -21,12 +21,14 @@ class Network(object):
 
     def __init__(self, output_node_names):
         self.output_node_names = output_node_names
+        #R: .graph is a DiGraph
         self.graph = OrderedGraph()  # preserving order is necessary for checkpointing
         self.freeze = False
         self.allow_neutral_mutations = False
         self.num_consecutive_mutations = 1
         self.switch = False
         self.direct_encoding = False
+        self.direct_global_feature = False
 
     def __deepcopy__(self, memo):
         """Override deepcopy to apply to class level attributes"""
@@ -45,7 +47,12 @@ class Network(object):
 class CPPN(Network):
     """A Compositional Pattern Producing Network"""
 
+    #The coordinates of each voxel are specified by x,y,z
+    # and radial distances from the center of the lattice workspace d. here, 8-by-8-by-7
+    # bias: b term set equal to 1 is taken as input to the network.
     input_node_names = ['x', 'y', 'z', 'd', 'b']
+
+    #functions of CPPN 
     activation_functions = [np.sin, np.abs, neg_abs, np.square, neg_square, sqrt_abs, neg_sqrt_abs]
 
     def __init__(self, output_node_names):
@@ -55,25 +62,25 @@ class CPPN(Network):
 
     def set_minimal_graph(self):
         """Create a simple graph with each input attached to each output"""
-        for name in self.input_node_names:
+        for name in self.input_node_names:                  #R: Cria os nodes de input
             self.graph.add_node(name, type="input", function=None)
 
-        for name in self.output_node_names:
+        for name in self.output_node_names:                 #R: Cria os nodes de output
             self.graph.add_node(name, type="output", function=sigmoid)
 
-        for input_node in nx.nodes(self.graph):
+        for input_node in nx.nodes(self.graph):             #R: Para cada node de input, conecta com um node de output com peso 0
             if self.graph.node[input_node]["type"] == "input":
                 for output_node in nx.nodes(self.graph):
                     if self.graph.node[output_node]["type"] == "output":
                         self.graph.add_edge(input_node, output_node, weight=0.0)
 
-    def set_input_node_states(self, orig_size_xyz):
+    def set_input_node_states(self, orig_size_xyz,bias=1): #R: Creates a input vector to each initial node (x,y,z,d,b)
         input_x = np.zeros(orig_size_xyz)
         input_y = np.zeros(orig_size_xyz)
         input_z = np.zeros(orig_size_xyz)
         for x in range(orig_size_xyz[0]):
             for y in range(orig_size_xyz[1]):
-                for z in range(orig_size_xyz[2]):
+                for z in range(orig_size_xyz[2]): 
                     input_x[x, y, z] = x
                     input_y[x, y, z] = y
                     input_z[x, y, z] = z
@@ -82,9 +89,12 @@ class CPPN(Network):
         input_y = normalize(input_y)
         input_z = normalize(input_z)
         input_d = normalize(np.power(np.power(input_x, 2) + np.power(input_y, 2) + np.power(input_z, 2), 0.5))
-        input_b = np.ones(orig_size_xyz)
+        if bias == 1:
+            input_b = np.ones(orig_size_xyz)
+        else:
+            input_b = np.ones(orig_size_xyz)*bias
 
-        for name in self.graph.nodes():
+        for name in self.graph.nodes():  #R: nodes in Di.Graph.nodes is like a dict [node,nodedata]
             if name == "x":
                 self.graph.node[name]["state"] = input_x
                 self.graph.node[name]["evaluated"] = True
@@ -103,11 +113,6 @@ class CPPN(Network):
 
     def mutate(self, num_random_node_adds=10, num_random_node_removals=0, num_random_link_adds=10,
                num_random_link_removals=5, num_random_activation_functions=100, num_random_weight_changes=100):
-
-        # TODO: control over initialization in __init__()
-        # TODO: set default arg val via brute force search
-        # TODO: weight std is defaulted to 0.5, to change this we can't just put it in args of mutate() b/c getargspec
-        # is used in create_new_children_through_mutation() to automatically pick the mutation type.
 
         variation_degree = None
         variation_type = None
@@ -144,7 +149,7 @@ class CPPN(Network):
     ###############################################
 
     def add_node(self):
-        # choose two random nodes (between which a link could exist)
+        #choose two random nodes (between which a link could exist)
         if len(self.graph.edges()) == 0:
             return "NoEdges"
         this_edge = random.choice(self.graph.edges())
@@ -337,6 +342,7 @@ class DirectEncoding(Network):
                 x, y, z = vox_xyz_from_id(idx, self.size)
                 self.mutable_vox[x, y, z] = False
 
+        #R: The values are random valuem from -1 to 1 in the size of the lattice: (x,y,z)
         if start_val is None:
             self.values = np.random.uniform(lower_bound, upper_bound, size=orig_size_xyz)
         else:
@@ -356,17 +362,6 @@ class DirectEncoding(Network):
 
         self.values = np.clip(self.values, self.lower_bound, self.upper_bound)
 
-    # @property
-    # def values(self):
-    #     return self._values
-    #
-    # @values.setter
-    # def values(self, values):
-    #     if self.func is None:
-    #         self._values = values
-    #     else:
-    #         self._values = self.func(values)
-
     def set_input_node_states(self, *args, **kwargs):
         pass
 
@@ -382,7 +377,7 @@ class DirectEncoding(Network):
 
         else:
             if rate is None:
-                rate = self.p
+                rate = self.p #p = 1/np.product(self.size, dtype='f')
 
             scale = self.scale
             if self.scale is None:
@@ -390,6 +385,7 @@ class DirectEncoding(Network):
                 # scale = np.clip(self.values**0.5, self.start_value**0.5, self.upper_bound)
                 # this was for meta mutations
 
+            #Selects the places where the mutation occurs
             selection = np.random.random(self.size) < rate
             selection = np.logical_and(selection, self.mutable_vox)
 
@@ -397,7 +393,7 @@ class DirectEncoding(Network):
                 change = np.random.choice(self.vox_options, self.size)
                 self.values[selection] = change[selection]
             else:
-                change = np.random.normal(scale=scale, size=self.size)
+                change = np.random.normal(scale=scale, size=self.size) #R: if the "standart" case, 
                 self.values[selection] += change[selection]
 
             self.values = np.clip(self.values, self.lower_bound, self.upper_bound)
@@ -411,9 +407,10 @@ class DirectEncoding(Network):
 
             return "gaussian", self.scale
 
+    #R: enforce_symmetry in x axis (x matrices are all equal)
     def enforce_symmetry(self):
-        if self.symmetric:
-            reversed_array = self.values[::-1, :, :]
+        if self.symmetric: #R: Just if the parammeter symmetric is True
+            reversed_array = self.values[::-1, :, :] #[::-1] means 'nothing for the first argument, nothing for the second, and jump by -1'
             self.values[:int(self.size[0]/2.0), :, :] = reversed_array[:int(self.size[0]/2.0), :, :]
 
     def regulate_sub_voxels(self):
@@ -431,269 +428,27 @@ class DirectEncoding(Network):
                 cx, cy, cz = vox_xyz_from_id(child, self.size)
                 self.values[cx, cy, cz] = group_val
 
+class DirectGlobalFeature(Network):
+    def __init__(self, output_node_names, feature_possibilities = [5e5,5e6,5e7]):
 
-class LearningTrialsContainer(Network):
+        Network.__init__(self, [output_node_names])
 
-    def __init__(self, name, learning_data=None):
+        self.direct_global_feature = True
+        self.feature_possibilities = feature_possibilities
+        self.feature = np.random.choice(self.feature_possibilities)
 
-        Network.__init__(self, [name])
-        self.direct_encoding = True
-        self.values = []
-        self.learning_data = learning_data
-
-    def update(self, values, *args):
-        self.values = values
-        return
-
-    def mutate(self, *args):
-        raise ArithmeticError
 
     def set_input_node_states(self, *args, **kwargs):
         pass
 
+    def mutate(self):
+        new_feature = self.feature
+        while new_feature == self.feature:
+            new_feature = np.random.choice(self.feature_possibilities)
 
-class PatchNet(Network):
+        self.feature = new_feature
 
-    def __init__(self, name, orig_geom, patch_mat, inputs, weights=None, num_hidden_neurons=0, input_bias=False,
-                 hidden_bias=False, evo_strategies=False, already_patched=False):
-
-        Network.__init__(self, [name])
-        self.direct_encoding = True
-        self.allow_neutral_mutations = False
-        self.size = orig_geom.shape
-        self.orig_geom = orig_geom
-        self.values = np.array(orig_geom)
-        self.patch_mat = patch_mat
-        self.evo_strategies = evo_strategies
-        self.already_patched = already_patched
-
-        # assert (len(inputs) == 3)  # roll, pitch, yaw
-        self.has_input_bias = input_bias
-        self.has_hidden_bias = hidden_bias
-        self.input_layer = inputs + [1.0]*input_bias
-        self.hidden_layer = [0]*num_hidden_neurons + [1]*hidden_bias
-        self.output_layer = [0]*3  # x, y, z
-
-        self.weights = weights
-        if weights is None:
-            if num_hidden_neurons > 0:
-                num_bias_weights = input_bias*num_hidden_neurons + hidden_bias*3
-                n = (len(inputs)+3)*num_hidden_neurons + num_bias_weights
-            elif evo_strategies:
-                n = len(self.output_layer)
-            else:
-                n = 3*(len(inputs)+input_bias)
-            self.weights = 2*np.random.rand(n)-1
-
-        self.update()
-
-    def update(self, orig_geom=None, inputs=None, loc=None):
-
-        if loc is not None:
-            self.values, _ = add_patch(orig_geom, loc=loc, mat=self.patch_mat)
-            return
-
-        if orig_geom is not None:
-            self.orig_geom = orig_geom
-
-        if self.already_patched or self.freeze:
-            self.values = self.orig_geom
-            return
-
-        if inputs is not None:
-            self.input_layer = inputs + [1.0]*self.has_input_bias
-
-        # track weight index
-        w = 0
-
-        if len(self.hidden_layer) > 0:
-
-            # hidden layer
-            new_hidden_layer = [0]*(len(self.hidden_layer) - self.has_hidden_bias)
-            for h in range(len(new_hidden_layer)):
-                for i in range(len(self.input_layer)):
-                    new_hidden_layer[h] += self.input_layer[i] * self.weights[w]
-                    w += 1
-
-            self.hidden_layer = list(np.tanh(new_hidden_layer)) + [1.0]*self.has_hidden_bias
-
-            # output layer
-            new_output_layer = [0]*len(self.output_layer)
-            for o in range(len(new_output_layer)):
-                for h in range(len(self.hidden_layer)):
-                    new_output_layer[o] += self.hidden_layer[h] * self.weights[w]
-                    w += 1
-
-            self.output_layer = np.tanh(new_output_layer)
-
-        elif self.evo_strategies:
-            self.output_layer = self.weights
-
-        else:  # no hidden layer input goes straight to output
-
-            # output layer
-            new_output_layer = [0]*len(self.output_layer)
-            for o in range(len(new_output_layer)):
-                for i in range(len(self.input_layer)):
-                    new_output_layer[o] += self.input_layer[i] * self.weights[w]
-                    w += 1
-
-            self.output_layer = np.tanh(new_output_layer)
-
-        shifted_output = 0.5*self.output_layer+0.5
-
-        # if self.evo_strategies:
-        #     w = 3
-        #     shifted_output = 0.5*shifted_output+0.2
-        #
-        #     if self.input_layer[0] == 0:
-        #         shifted_output[0] = 1-(shifted_output[0]+0.1)
-        #         shifted_output[1] = 1-(shifted_output[1]+0.1)
-        #
-        #     if self.input_layer[0] == 1:
-        #         shifted_output[0] = 1-(shifted_output[0]+0.1)
-        #
-        #     if self.input_layer[0] == 2:
-        #         shifted_output[1] = 1-(shifted_output[1]+0.1)
-
-        loc = [0, 0, 0]
-        for i in range(3):
-            loc[i] = self.size[i]*shifted_output[i]
-
-        # new geometry: just a single patch different from original geometry
-        self.values, _ = add_patch(self.orig_geom, loc=loc, mat=self.patch_mat)
-
-        if w < len(self.weights):
-            n = len(self.weights) - w
-            print "only need the first {0} weights (deleting the extra {1})".format(w, n)
-            self.weights = self.weights[:w]
-
-    def mutate(self, *args):
-        new = np.array(self.weights)
-        selection = np.random.rand(new.size) < 1/float(new.size)
-        change = np.random.normal(scale=new*new, size=new.size)
-        new[selection] += change[selection]
-        new = np.clip(new, -1, 1)
-        self.weights = list(new)
-        self.update()
-        return "normal", 1
-
-    def set_input_node_states(self, *args, **kwargs):
-        pass
+        return "Different_global_stiffnes",self.feature
 
 
-class PatchCppn(CPPN):
-    def __init__(self, name, orig_geom, patch_mat, inputs):
-
-        Network.__init__(self, ["x", "y", "z", name])  # dummy node "name" to work with g->p mapping
-
-        self.input_node_names = ["roll", "pitch", "yaw"]
-        assert (len(inputs) == 3)  # roll, pitch, yaw
-        self.inputs = inputs
-
-        self.direct_encoding = True
-        self.allow_neutral_mutations = False
-        self.size = orig_geom.shape
-        self.orig_geom = orig_geom
-        self.values = np.array(orig_geom)
-        self.patch_mat = patch_mat
-
-        self.set_minimal_graph()
-        super(PatchCppn, self).mutate()
-        self.update()
-
-    def set_input_node_states(self, *args):
-        for name in self.graph.nodes():
-            if name == "roll":
-                self.graph.node[name]["state"] = self.inputs[0]
-                self.graph.node[name]["evaluated"] = True
-            if name == "pitch":
-                self.graph.node[name]["state"] = self.inputs[1]
-                self.graph.node[name]["evaluated"] = True
-            if name == "yaw":
-                self.graph.node[name]["state"] = self.inputs[2]
-                self.graph.node[name]["evaluated"] = True
-            if name == "bias":
-                self.graph.node[name]["state"] = 1.0
-                self.graph.node[name]["evaluated"] = True
-
-    def calc_node_state(self, node_name):
-        if self.graph.node[node_name]["evaluated"]:
-            return self.graph.node[node_name]["state"]
-
-        self.graph.node[node_name]["evaluated"] = True
-        input_edges = self.graph.in_edges(nbunch=[node_name])
-        new_state = 0
-
-        for edge in input_edges:
-            node1, node2 = edge
-            new_state += self.calc_node_state(node1) * self.graph.edge[node1][node2]["weight"]
-
-        self.graph.node[node_name]["state"] = new_state
-
-        return self.graph.node[node_name]["function"](new_state)
-
-    def update(self, orig_geom=None, inputs=None):
-
-        if orig_geom is not None:
-            self.orig_geom = orig_geom
-
-        if inputs is not None:
-            self.inputs = inputs
-
-        for name in self.graph.nodes():
-            self.graph.node[name]["evaluated"] = False  # flag all nodes as unevaluated
-
-        self.set_input_node_states(self.inputs)  # reset the inputs
-
-        for name in self.output_node_names:
-            self.graph.node[name]["state"] = 0.0  # clear old outputs
-            self.graph.node[name]["state"] = self.calc_node_state(name)  # calculate new outputs
-
-        loc = [0, 0, 0]
-        for n, name in enumerate(self.output_node_names[:3]):
-            loc[n] = self.size[n] * (0.5*self.graph.node[name]["state"] + 0.5)
-
-        # print self.size
-        # print "loc: ", loc
-
-        # new geometry: just a single patch different from original geometry
-        self.values, _ = add_patch(self.orig_geom, loc=loc, mat=self.patch_mat)
-
-    def mutate(self, *args):
-        mut_func_args = [0 for _ in range(6)]
-        choice = random.choice(range(6))
-        mut_func_args[choice] = 1
-        super(PatchCppn, self).mutate(*mut_func_args)
-        self.update()
-
-
-class GeneralizedCPPN(CPPN):
-
-    def __init__(self, output_node_names, include_default_inputs=True, **additional_inputs):
-        CPPN.__init__(self, output_node_names)
-
-        if not include_default_inputs:
-            self.input_node_names = []
-
-        self.additional_inputs_dict = additional_inputs
-
-        for key, val in additional_inputs.items():
-            if key not in self.input_node_names:
-                self.input_node_names += [key]
-
-        self.graph = OrderedGraph()  # reset
-        super(GeneralizedCPPN, self).set_minimal_graph()
-        super(GeneralizedCPPN, self).mutate()
-
-    def set_input_node_states(self, orig_size_xyz):
-
-        super(GeneralizedCPPN, self).set_input_node_states(orig_size_xyz)
-
-        for name in self.graph.nodes():
-
-            for key, val in self.additional_inputs_dict.items():
-                if name == key:
-                    self.graph.node[name]["state"] = val
-                    self.graph.node[name]["evaluated"] = True
-
+        
